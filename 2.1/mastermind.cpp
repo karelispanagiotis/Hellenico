fe5ab7f782fb4b0521e7ab8fe2973e5064180869
Panagiotis Karelis <karelispanagiotis@gmail.com>
2021-04-26 22:58:12 +0300

    Improved 2.1/mastermind

--- 2.1/mastermind.cpp
+++ 2.1/mastermind.cpp
@@ -1,70 +1,55 @@
-#include <cstdio>
 #include <bits/stdc++.h>
+using namespace std;
 
-int n, m, k, ans, in_data[10][7], sequence[5];
+int N, M, K, ans, restrictions[15][12], sequence[10];
 
 bool isOk()
 {
-    int cpy_in[10][7], copy_seq[5];
-    memcpy(cpy_in, in_data, 10*7*sizeof(int));
-
-    for(int a=0; a<n; a++)
-    {
-        memcpy(copy_seq, sequence, 5*sizeof(int));
-        int unmoved = 0;
-        for(int i=0; i<k; i++)
-        {
-            if(cpy_in[a][i]==copy_seq[i])
-            {
-                copy_seq[i] = cpy_in[a][i] = 0;
-                unmoved++;
-            }
-        }
-
-        int permutated = 0;
-        for(int i=0; i<k; i++)
-        {
-            for(int j=0; j<k; j++)  //Search copy_seq
-            {
-                if(copy_seq[j]==cpy_in[a][i] && copy_seq[j]!=0)
-                {
-                    copy_seq[j] = cpy_in[a][i] = 0;
-                    permutated++;
-                }
-            }
-        }
-        if(unmoved!=cpy_in[a][k] || permutated!=cpy_in[a][k+1])
-            return false;
-    }
-    return true;
+	for(int i=0; i<N; i++)
+	{
+		int correct=0, permuted=0;
+		vector<int> emf(15);
+		for(int j=0; j<K; j++)
+			if(sequence[j] == restrictions[i][j]) correct++;
+			else emf[restrictions[i][j]]++;
+		for(int j=0; j<K; j++)
+		{
+			if(sequence[j] == restrictions[i][j]) continue;
+			if(emf[sequence[j]] > 0)
+				permuted++, emf[sequence[j]]--;
+		}
+		if(correct!=restrictions[i][K] || permuted!=restrictions[i][K+1])
+			return false;
+	}
+	return true;
 }
 
 void search(int pos)
 {
-    if(pos==k)
-    {
-        if(isOk()) ans++;
-        return;
-    }
-    for(int i=1; i<=m; i++)
-    {
-        sequence[pos] = i;
-        search(pos+1);
-    }
+	if(pos==K)
+	{
+		if(isOk()) ans++;
+		return;
+	}
+
+	for(int i=1; i<=M; i++)
+	{
+		sequence[pos] = i;
+		search(pos+1);
+	}
+
 }
 
 int main()
 {
-    freopen("mastermind.in", "r", stdin);
-    freopen("mastermind.out", "w", stdout);
+	freopen("mastermind.in", "r", stdin);
+	freopen("mastermind.out", "w", stdout);
+	scanf("%d%d%d", &N, &M, &K);
+	for(int i=0; i<N; i++)
+		for(int j=0; j<K+2; j++)
+			scanf("%d", &restrictions[i][j]);
+	search(0);
+	printf("%d\n", ans);
 
-    scanf("%d %d %d", &n, &m, &k);
-    for(int i=0; i<n; i++)
-        for(int j=0; j<k+2; j++)
-            scanf("%d", &in_data[i][j]);
-
-    search(0);
-
-    printf("%d", ans);
-    return 0;
+	return 0;
 }
